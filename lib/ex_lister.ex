38da4c31ad89f32bf3c5f2dace37f1c26dcee34b
Pavel Vlasikhin <pavel@vlasikhin.com>
2024-10-27 14:19:49 +0100

    Initial commit

---
+++ lib/ex_lister.ex
@@ -0,0 +1,18 @@
+defmodule ExLister do
+  @moduledoc """
+  Documentation for `ExLister`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExLister.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_lister_test.exs
@@ -0,0 +1,8 @@
+defmodule ExListerTest do
+  use ExUnit.Case
+  doctest ExLister
+
+  test "greets the world" do
+    assert ExLister.hello() == :world
+  end
+end
